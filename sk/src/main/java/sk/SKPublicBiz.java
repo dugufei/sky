10c36dc1774cc3505facb54d7e7ca76502489ac5
金灿 <jincan01@iqiyi.com>
2019-02-26 11:22:22 +0800

    公共biz

--- sk/src/main/java/sk/SKPublicBiz.java
+++ sk/src/main/java/sk/SKPublicBiz.java
@@ -1,7 +1,12 @@
 package sk;
 
+import android.os.Bundle;
+
 /**
  * @author sky
  * @version 版本
  */
-public abstract class SKPublicBiz extends SKBiz {}
+public abstract class SKPublicBiz extends SKBiz {
+
+	@Override public void initBiz(Bundle bundle) {}
+}
